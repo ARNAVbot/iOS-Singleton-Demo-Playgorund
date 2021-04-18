6343d3921f4171b384f6bfce0ee9d96a14f2e2d3
@arnavagarwal <alnico.arnav@gmail.com>
2021-04-19 03:00:51 +0530

    First commit

---
+++ SingletonDemo.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class Car {
+    var color = "Red"
+    
+    static let singletonCar = Car()
+}
+
+let myCar = Car.singletonCar
+myCar.color = "Blue"
+
+let yourCar = Car.singletonCar
+print(yourCar.color)
+
+
+// in the same way that we created above singleton instances of car , we do the same for 
